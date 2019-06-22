c994549b904ea7bd409df9d627451f9e4afaeb09
Ubuntu <ubuntu@ip-172-31-84-91.ec2.internal>
2019-06-22 03:49:22 +0000

    Modified to fit server

--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = 'f53c29c05cf7c240d81618abec3c0b21'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'ahuang773@gmail.com'
-    MAIL_PASSWORD = 'HireMe4Job!'
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
