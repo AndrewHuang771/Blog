2b12615d527e482b34194cfc763bc1bfeb7c5172
Andrew Huang <ahuang771@gmail.com>
2019-06-23 18:47:28 -0400

    Made cool home page like pinterest

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,5 +6,5 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def root():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
     return render_template('home.html', posts=posts)
--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,7 +1,41 @@
+const top_padding = 25;
+const post_width = 220;
+
+function movePost(node, column, columnHeight, tables) {
+  node.style.top = columnHeight + 'px';
+  node.style.left = (column * (post_width + top_padding)) + 'px';
+  node.style.display = 'inline-block';
+  tables[column] += parseInt(node.style.height.substr(0, node.style.height.length - 2)) + top_padding;
+}
+
+function findShortestCol(tables) {
+  let minInfo = {
+    min: tables[0],
+    idx: 0,
+  };
+  for ( let i = 0; i < tables.length; i ++ ) {
+    if ( tables[i] < minInfo.min ) {
+      minInfo.min = tables[i];
+      minInfo.idx = i;
+    }
+  }
+  return minInfo;
+}
+
 $(document).ready(function() {
+  const tables = [];
+  const numCols = Math.floor($(".wrapper-post").width()/220);
+  for ( let i = 0; i < numCols; i ++ ) {
+    tables.push(top_padding);
+  }
+
   $('.wrapper-post').children('a').each(function() {
     this.style.height = 100*(Math.ceil(Math.random()*3+2)) + 'px';
+    let col = findShortestCol(tables);
+    movePost(this, col.idx, col.min, tables);
   });
+
+  //Now sort them into columns.
 
   $(".post-regular").on('mouseenter', function() {
     $(this).children('.post-content')[0].style.left = 0 + 'px';
